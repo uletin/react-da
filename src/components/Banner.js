720554b89e3b8bd2231cf222c2ae226db8698dec
uletin <dokterkoding111>
2022-07-13 08:58:38 +0800

    belajar react js hari pertama

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Banner = () => {
+    return(
+        <h5>Banner Gambar Kampus</h5>
+    )
+}
+
+export default Banner
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const NavigationBar = () => {
+    return(
+        <h1>Ini adalah Navigation Bar</h1>
+    )
+}
+
+export default NavigationBar
